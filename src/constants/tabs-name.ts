558f576494fed201220e886bf88c9a528a4a3d0c
ishan-gurao <ishan.g@maxscriptstechnologies.com>
2025-08-25 14:06:56 +0530

    Tabs renderd

---
+++ src/constants/tabs-name.ts
@@ -0,0 +1,11 @@
+import { localization } from "../localization/localization";
+
+export const tabs = [
+  { label: localization.NEW, value: "1" },
+  { label: localization.DRAFTS, value: "2" },
+  { label: localization.QUOTES, value: "3" },
+  { label: localization.WILL_CALL, value: "4" },
+  { label: localization.SPECIAL_ORDERS, value: "5" },
+  { label: localization.COMPLETED, value: "6" },
+  { label: localization.VOIDED, value: "7" },
+];
---
+++ src/localization/localization.ts
@@ -0,0 +1,10 @@
+export const localization ={
+    TRANSACTIONS: "Transactions",
+    NEW: "New",
+    DRAFTS: "Drafts",
+    QUOTES: "Quotes",
+    WILL_CALL:"Will Call",
+    SPECIAL_ORDERS:"Special Orders",
+    COMPLETED:"Completed",
+    VOIDED:"Voided",
+}
